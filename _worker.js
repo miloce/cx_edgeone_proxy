92a978e2dc6a546e0f67e7e76d9d09712ee15c2f
Miloce <microne@qq.com>
2026-04-20 21:37:15 +0800

    Add files via upload

---
+++ _worker.js
@@ -0,0 +1,27 @@
+addEventListener('fetch', async event => {
+    event.respondWith(await _fetch(event.request));
+});
+
+async function _fetch(request) {
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/proxy/')) {
+        url.pathname = url.pathname.replace('/proxy', '');
+        url.hostname = 'mobilelearn.chaoxing.com';
+
+        let newRequest = new Request(url, {
+            method: request.method,
+            headers: request.headers,
+            body: request.body,
+            redirect: 'follow'
+        });
+
+        newRequest.headers.set('Host', 'mobilelearn.chaoxing.com');
+        newRequest.headers.set('Referer', 'https://mobilelearn.chaoxing.com');
+        newRequest.headers.set('Origin', 'https://mobilelearn.chaoxing.com');
+        newRequest.headers.set('User-Agent', 'Mozilla/5.0 (iPhone Mac OS X) github.com/misaka-1314');
+
+        return fetch(newRequest);
+    } else {
+        return new Response('Powered by Misaka! github.com/misaka-1314');
+    }
+}
